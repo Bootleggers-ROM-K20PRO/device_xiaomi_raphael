23d91415686934569503327547b166902d56e785
taran107 <hora.cooltaran@gmail.com>
2020-05-15 19:49:44 +0530

    Bootleggers Bringup

---
+++ bootleg_raphael.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/raphael/device.mk)
+
+# Inherit some common Bootleggers stuff.
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+TARGET_INCLUDE_WIFI_EXT := true
+$(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := bootleg_raphael
+PRODUCT_DEVICE := raphael
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi K20 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+BUILD_FINGERPRINT := "Xiaomi/raphael/raphael:10/QKQ1.190825.002/V11.0.2.0.QFKMIXM:user/release-keys"
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
