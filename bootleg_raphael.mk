74ceeb1573e83c2f7209ca10905962614dd1a414
taran107 <hora.cooltaran@gmail.com>
2020-05-16 18:57:26 +0530

    Some Changes

--- bootleg_raphael.mk
+++ bootleg_raphael.mk
@@ -7,22 +7,19 @@
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
 # Inherit some common Bootleggers stuff.
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_INCLUDE_WIFI_EXT := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := false
 $(call inherit-product, vendor/bootleggers/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := bootleg_raphael
-PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi K20 Pro
+PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_NAME := bootleg_raphael
 
-BUILD_FINGERPRINT := "Xiaomi/raphael/raphael:10/QKQ1.190825.002/V11.0.2.0.QFKMIXM:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BOOTLEGGERS_SITDOWN := true
 
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
